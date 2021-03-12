a43509342792121fa497c538cfc7881f2234f3cc
肖家豪 <xiaojiahao@haodf.com>
2021-03-12 09:45:42 +0800

    2021-03-12 整数反转

---
+++ src/main/java/com/wy/athena/simple/No002Solution.java
@@ -0,0 +1,59 @@
+package com.wy.athena.simple;
+
+/**
+ * 描述:
+ * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ *
+ * 如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+ *
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：x = 123
+ * 输出：321
+ * 示例 2：
+ *
+ * 输入：x = -123
+ * 输出：-321
+ * 示例 3：
+ *
+ * 输入：x = 120
+ * 输出：21
+ * 示例 4：
+ *
+ * 输入：x = 0
+ * 输出：0
+ *  
+ *
+ * 提示：
+ *
+ * -231 <= x <= 231 - 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author xjh
+ * created on 2021-03-12 9:37 上午
+ */
+public class No002Solution {
+    public static void main(String[] args) {
+        System.out.println(reverse(-100));
+    }
+
+
+    public static int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            if ((ans * 10) / 10 != ans) {
+                ans = 0;
+                break;
+            }
+            ans = ans * 10 + x % 10;
+            x = x / 10;
+        }
+        return ans;
+    }
+}
