91689afea81f8a199ca267e1184408b333d8aecc
肖家豪 <xiaojiahao@haodf.com>
2021-03-12 09:35:15 +0800

    Initial commit

---
+++ src/main/java/com/wy/athena/AthenaApplication.java
@@ -0,0 +1,16 @@
+package com.wy.athena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xjh
+ */
+@SpringBootApplication
+public class AthenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaApplication.class, args);
+    }
+
+}
