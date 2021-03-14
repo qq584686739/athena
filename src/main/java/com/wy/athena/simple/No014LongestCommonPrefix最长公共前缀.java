9b2488ad457bc98a11b8fff2cb2f6648e26ad953
肖家豪 <xiaojiahao@haodf.com>
2021-03-14 21:26:50 +0800

    2021-03-14 最长公共前缀

---
+++ src/main/java/com/wy/athena/simple/No014LongestCommonPrefix最长公共前缀.java
@@ -0,0 +1,67 @@
+package com.wy.athena.simple;
+
+/**
+ * 描述:
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ * 示例 2：
+ *
+ * 输入：strs = ["dog","racecar","car"]
+ * 输出：""
+ * 解释：输入不存在公共前缀。
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] 仅由小写英文字母组成
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-common-prefix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author xjh
+ * created on 2021-03-14 9:21 下午
+ */
+public class No014LongestCommonPrefix最长公共前缀 {
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"abc", "aaaaaa","auau"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public static String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+
+
+}
