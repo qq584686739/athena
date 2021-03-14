68ac04b63ace22523d43dd01d46fc240d1f24bdb
肖家豪 <xiaojiahao@haodf.com>
2021-03-14 21:35:36 +0800

    2021-03-14 有效的括号

---
+++ src/main/java/com/wy/athena/simple/No020IsValid有效的括号.java
@@ -0,0 +1,84 @@
+package com.wy.athena.simple;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 描述:
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "()"
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * 示例 3：
+ *
+ * 输入：s = "(]"
+ * 输出：false
+ * 示例 4：
+ *
+ * 输入：s = "([)]"
+ * 输出：false
+ * 示例 5：
+ *
+ * 输入：s = "{[]}"
+ * 输出：true
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 104
+ * s 仅由括号 '()[]{}' 组成
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author xjh
+ * created on 2021-03-14 9:29 下午
+ */
+public class No020IsValid有效的括号 {
+    public static void main(String[] args) {
+        System.out.println(isValid("(((([]))))"));
+    }
+
+    public static boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || !stack.peek().equals(pairs.get(ch))) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
