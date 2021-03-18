2ffe316c4d4c2f8bf607d4dadc52ffb931c2b7cc
肖家豪 <xiaojiahao@haodf.com>
2021-03-18 20:43:02 +0800

    2021-03-17 最大子序和

---
+++ src/main/java/com/wy/athena/simple/No053maxSubArray最大子序和.java
@@ -0,0 +1,96 @@
+package com.wy.athena.simple;
+
+/**
+ * 描述:
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 示例 2：
+ *
+ * 输入：nums = [1]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：nums = [0]
+ * 输出：0
+ * 示例 4：
+ *
+ * 输入：nums = [-1]
+ * 输出：-1
+ * 示例 5：
+ *
+ * 输入：nums = [-100000]
+ * 输出：-100000
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -105 <= nums[i] <= 105
+ *  
+ *
+ * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author xjh
+ * created on 2021-03-18 2:38 下午
+ */
+public class No053maxSubArray最大子序和 {
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray1(new int[]{-1, 2, -1, 2, 3, 4, 5}));
+    }
+
+    public static int maxSubArray1(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+
+    public static class Status {
+        public int lSum, rSum, mSum, iSum;
+
+        public Status(int lSum, int rSum, int mSum, int iSum) {
+            this.lSum = lSum;
+            this.rSum = rSum;
+            this.mSum = mSum;
+            this.iSum = iSum;
+        }
+    }
+
+    public static int maxSubArray2(int[] nums) {
+        return getInfo(nums, 0, nums.length - 1).mSum;
+    }
+
+    public static Status getInfo(int[] a, int l, int r) {
+        if (l == r) {
+            return new Status(a[l], a[l], a[l], a[l]);
+        }
+        int m = (l + r) >> 1;
+        Status lSub = getInfo(a, l, m);
+        Status rSub = getInfo(a, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
+    public static Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = Math.max(l.lSum, l.iSum + r.lSum);
+        int rSum = Math.max(r.rSum, r.iSum + l.rSum);
+        int mSum = Math.max(Math.max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return new Status(lSum, rSum, mSum, iSum);
+    }
+
+}
