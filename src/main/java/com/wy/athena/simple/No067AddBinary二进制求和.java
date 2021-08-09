1fa9894820f7d1b1e2236b15d8bd7ada235acd29
肖家豪 <xiaojiahao@haodf.com>
2021-08-09 16:58:04 +0800

    2021-08-09 二进制求和

---
+++ src/main/java/com/wy/athena/simple/No067AddBinary二进制求和.java
@@ -0,0 +1,52 @@
+package com.wy.athena.simple;
+
+/**
+ * 描述:
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ *
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * 示例 2:
+ *
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ *  
+ *
+ * 提示：
+ *
+ * 每个字符串仅由字符 '0' 或 '1' 组成。
+ * 1 <= a.length, b.length <= 10^4
+ * 字符串如果不是 "0" ，就都不含前导零。
+ *
+ * 解决思路：整体思路是将两个字符串较短的用 00 补齐，使得两个字符串长度一致，然后从末尾进行遍历计算，得到最终结果。
+ *
+ * @author xjh
+ * created on 2021-08-09 4:04 下午
+ */
+public class No067AddBinary二进制求和 {
+    public static void main(String[] args) {
+
+    }
+    public static String addBinary(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+        int ca = 0;
+        for(int i = a.length() - 1, j = b.length() - 1;i >= 0 || j >= 0; i--, j--) {
+            int sum = ca;
+            sum += i >= 0 ? a.charAt(i) - '0' : 0;
+            sum += j >= 0 ? b.charAt(j) - '0' : 0;
+            ans.append(sum % 2);
+            ca = sum / 2;
+        }
+        ans.append(ca == 1 ? ca : "");
+        return ans.reverse().toString();
+    }
+
+
+
+}
